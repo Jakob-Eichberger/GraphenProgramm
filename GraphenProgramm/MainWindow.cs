76480bd188782251da6bc8e069b0e28d93de4033
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-02-13 21:45:59 +0100

    Renamed Form 1 to MainWindow

--- GraphenProgramm/MainWindow.cs
+++ GraphenProgramm/MainWindow.cs
@@ -21,7 +21,7 @@ Function that shows all the output in a grid     <- Not Done
 
 namespace GrafenProgramm
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
 
     {
         MatrixClass matrix = new MatrixClass();
@@ -33,7 +33,7 @@ namespace GrafenProgramm
         string AD;
         string wegmatrix;
 
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
 
